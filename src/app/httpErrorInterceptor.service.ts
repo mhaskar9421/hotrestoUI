1fdb4f0805e280e73bb7ebb504d2aa8e0a14e852
sachin9421002025 <sachin.mhaskar7@gmail.com>
2019-07-27 11:45:49 +0530

    data tables issue resolved

--- src/app/httpErrorInterceptor.service.ts
+++ src/app/httpErrorInterceptor.service.ts
@@ -5,16 +5,16 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
- 
+
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthService) {}
- 
+    constructor(private authenticationService: AuthService) { }
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
             if (err.status === 401 || err.status === 403) {
                 // auto logout if 401 response returned from api
-                this.authenticationService.logout();
+                //this.authenticationService.logout();
                 // location.reload(true);
             }
             return throwError(err);
