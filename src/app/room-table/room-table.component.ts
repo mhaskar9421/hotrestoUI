64a874e5c4bd7e59067989509a33d22b7c38d2ce
sachin9421002025 <sachin.mhaskar7@gmail.com>
2019-08-25 00:16:23 +0530

    book room

--- src/app/room-table/room-table.component.ts
+++ src/app/room-table/room-table.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { RoomService } from '../rooms/room.service';
+import { RoomTableService } from './room-table.service';
 import { MatSnackBar } from '@angular/material';
 import { notificationMessages } from '../../notificationMessages';
 
 @Component({
   selector: 'app-room-table',
   templateUrl: './room-table.component.html',
-  styleUrls: ['./room-table.component.scss']
+  styleUrls: ['./room-table.component.scss'],
+  providers: [RoomTableService]
 })
 export class RoomTableComponent implements OnInit {
   @Input() tabTitle: string;
+  @Input() activeTab: string;
   loading = false;
   roomList: {};
-  constructor(private _snackBar: MatSnackBar, private roomService: RoomService, public constants: notificationMessages) { }
+  constructor(private _snackBar: MatSnackBar, private roomTableService: RoomTableService, public constants: notificationMessages) { }
 
   ngOnInit() {
-    this.viewRooms();
+    if (this.activeTab != 'book-room') { this.viewRooms(); }
   }
 
   viewRooms() {
     this.loading = true;
-    this.roomService.viewRoom()
+    this.roomTableService.viewRoom()
       .subscribe(
         data => {
           if (data) {
@@ -38,7 +40,7 @@ export class RoomTableComponent implements OnInit {
   }
 
   deleteRoom(item) {
-    this.roomService.deleteRoom(item)
+    this.roomTableService.deleteRoom(item)
       .subscribe(data => {
         this._snackBar.open(this.constants.deleteRoom, '', {
           duration: 3000,
---
+++ src/app/room-table/room-table.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operator/map';
+import { environment } from '../../environments/environment';
+import 'rxjs/Rx';
+
+@Injectable()
+export class RoomTableService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    viewRoom() {
+        return this.http.get(environment.BACKEND.URL.viewRoom)
+            .map(res => {
+                return res;
+            });
+    }
+
+    deleteRoom(room_id: number) {
+        return this.http.get(environment.BACKEND.URL.deleteRoom + room_id)
+            .map(res => {
+                return res;
+            })
+    }
+
+}
