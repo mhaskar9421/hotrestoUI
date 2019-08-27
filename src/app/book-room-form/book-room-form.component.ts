e0906bd7e3e51f4c10fe784b1422feb65c638af1
deeptimhaskar <deeptiagre.mca@gmail.com>
2019-08-28 00:53:29 +0530

    chckout and book-room form

--- src/app/book-room-form/book-room-form.component.ts
+++ src/app/book-room-form/book-room-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, Output, EventEmitter, Input, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material';
 
 @Component({
   selector: 'app-book-room-form',
@@ -12,22 +13,42 @@ export class BookRoomFormComponent implements OnInit {
   thirdFormGroup: FormGroup;
   formView: boolean = false;
   @Input() activeTab: string;
+  @ViewChild('stepper') stepper: MatStepper;
+  isEditable = true;
   @Output() formEvent = new EventEmitter<boolean>();
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.firstFormGroup = this.formBuilder.group({
-      firstCtrl: ['', Validators.required]
+      name: ['', Validators.required],
+      idtype: ['', Validators.required],
+      idnumber: ['', Validators.required],
+      phonenumber: ['', Validators.required],
+      address: ['', Validators.required],
+      uploadid: ['', Validators.required]
     });
     this.secondFormGroup = this.formBuilder.group({
-      secondCtrl: ['', Validators.required]
+      roomnumber: ['', Validators.required],
+      noofdays: ['', Validators.required],
+      roomamount: ['', Validators.required],
+      extraoccupancy: ['', Validators.required],
+      totalamount: ['', Validators.required]
     });
     this.thirdFormGroup = this.formBuilder.group({
-      thirdCtrl: ['', Validators.required]
+      billamount: ['', Validators.required],
+      paymenttype: ['', Validators.required],
+      paidamount: ['', Validators.required],
+      paymentstatus: ['', Validators.required]
     });
   }
+  onSubmit() {
+    console.log("submit the form");
+  }
 
+  move(index: number) {
+    this.stepper.selectedIndex = index;
+  }
   callRoomTable() {
     this.formEvent.emit(this.formView);
     localStorage.setItem('showRoomList', 'true');
