0e99d2af521bc857cf0b47d0afcd506eb47443fb
sachin9421002025 <sachin.mhaskar7@gmail.com>
2019-08-28 15:49:42 +0530

    checkout comp

--- src/app/checkout-form/checkout-form.component.ts
+++ src/app/checkout-form/checkout-form.component.ts
@@ -16,13 +16,11 @@ export class CheckoutFormComponent implements OnInit {
   printInvoice(printArea) {
     var printContents = document.getElementById(printArea).innerHTML;
     var originalContents = document.body.innerHTML;
-
     document.body.innerHTML = printContents;
-
     window.print();
-
     document.body.innerHTML = originalContents;
   }
+
   callCustomerList() {
     this.checkoutEvent.emit(this.showCheckout);
   }
