316e45c93796fa5ae2dccd3a89a8f544e01ea6f2
deeptimhaskar <deeptiagre.mca@gmail.com>
2019-09-02 13:38:18 +0530

    checkout form

--- src/app/checkout-form/checkout-form.component.ts
+++ src/app/checkout-form/checkout-form.component.ts
@@ -7,22 +7,26 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class CheckoutFormComponent implements OnInit {
   showCheckout: boolean = false;
+  document: Document;
   @Output() checkoutEvent = new EventEmitter<boolean>();
   constructor() { }
 
   ngOnInit() {
   }
 
-  sync(amount) {
-    var amount1 = (<HTMLInputElement>document.getElementById('amount')).value;
-  }
-
   printInvoice(printArea) {
+    //   var divToPrint = document.getElementById('printArea');
+    //   newWin.document.write('<button onload="window.print()">' + printArea.innerHTML + '</button>');
+    //   newWin.document.close();
+    //   setTimeout(function () { newWin.close(); }, 10);
+    var input = (<HTMLInputElement>document.getElementById('amount')).value;
     var printContents = document.getElementById(printArea).innerHTML;
     var originalContents = document.body.innerHTML;
     document.body.innerHTML = printContents;
     window.print();
+    console.log(originalContents);
     document.body.innerHTML = originalContents;
+    (<HTMLInputElement>document.getElementById('amount')).value = input;
   }
 
   callCustomerList() {
