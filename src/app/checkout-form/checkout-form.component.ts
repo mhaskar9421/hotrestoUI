01a7d2b26c975e8bc2248ce43d54e3d53f0bfd11
sachin9421002025 <sachin.mhaskar7@gmail.com>
2019-09-09 23:43:05 +0530

    final for day

--- src/app/checkout-form/checkout-form.component.ts
+++ src/app/checkout-form/checkout-form.component.ts
@@ -9,7 +9,6 @@ import { DataService } from "../data.service";
 })
 export class CheckoutFormComponent implements OnInit {
   showCheckout: boolean = false;
-  subTotal = 0;
   finalTotal = 0;
   document: Document;
   taxData: {};
@@ -25,10 +24,14 @@ export class CheckoutFormComponent implements OnInit {
   roomObject: {};
   room_id: number;
   customer_id: number;
+  bookingdate: string;
+  invoiceNo: string;
   @Output() checkoutEvent = new EventEmitter<boolean>();
   constructor(private data: DataService, private gettaxService: getTaxService) { }
 
   ngOnInit() {
+    this.invoiceNo = this.checkoutData['booking_id'];
+    this.bookingdate = this.checkoutData['booking_date'];
     this.roomCharges = this.checkoutData['room_charges'];
     this.extraOccupancy = this.checkoutData['extra_occupancy'];
     this.foodBillNumber = this.checkoutData['food_bill_number'];
@@ -37,16 +40,13 @@ export class CheckoutFormComponent implements OnInit {
     this.finalTotal = this.checkoutData['total_amount'];
     this.getTaxAmount();
     this.getRoomCustDetails();
-    // this.subTotal = parseInt(this.checkoutData['room_charges']) + parseInt(this.checkoutData['extra_occupancy']);
   }
 
   printInvoice(printArea) {
     var printContents = document.getElementById(printArea).innerHTML;
     var originalContents = document.body.innerHTML;
-    // document.body.innerHTML = input;
     document.body.innerHTML = printContents;
     window.print();
-
     document.body.innerHTML = originalContents;
   }
 
@@ -57,32 +57,31 @@ export class CheckoutFormComponent implements OnInit {
   getTaxAmount() {
     this.gettaxService.getTaxData()
       .subscribe(
-      data => {
-        if (data) {
-          this.taxData = data[0];
-          this.taxAmount = this.taxData['tax_amount'];
-          this.finalTotal = this.subTotal + (this.subTotal * parseInt(this.taxData['tax_amount']) / 100);
-        } else {
-          this.taxData = 0;
-        }
-      },
-      error => {
-        console.log(error);
-      });
+        data => {
+          if (data) {
+            this.taxData = data[0];
+            this.taxAmount = this.taxData['tax_amount'];
+          } else {
+            this.taxData = 0;
+          }
+        },
+        error => {
+          console.log(error);
+        });
   }
 
   getRoomCustDetails() {
     this.gettaxService.getRoomCustDetails(this.room_id, this.customer_id)
       .subscribe(
-      data => {
-        if (data) {
-          this.roomObject = data;
-        } else {
-          this.roomObject = null;
-        }
-      },
-      error => {
-        console.log(error);
-      });
+        data => {
+          if (data) {
+            this.roomObject = data;
+          } else {
+            this.roomObject = null;
+          }
+        },
+        error => {
+          console.log(error);
+        });
   }
 }
